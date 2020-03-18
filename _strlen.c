3f3ee373f6c26c9fcf07bc2b0525c906abf57739
peluza <edisonisaza@gmail.com>
2020-03-18 12:20:23 -0500

    account_chars

--- _strlen.c
+++ _strlen.c
@@ -8,13 +8,14 @@
  */
 int _strlen(char *s)
 {
-    char a;
-    int x, c;
-    a = s[0];
-    for (x = 0; a != '\0'; x++)
-    {
-        a = s[x];
-        c = x;
-    }
-return (c);
+	char a;
+	int x, c;
+
+	a = s[0];
+	for (x = 0; a != '\0'; x++)
+	{
+		a = s[x];
+		c = x;
+	}
+	return (c);
 }
