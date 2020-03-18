bdc5cb12a012a22f1b38bd7d7a7f4ba1709b9a9c
peluza <edisonisaza@gmail.com>
2020-03-18 12:19:28 -0500

    header

--- holberton.h
+++ holberton.h
@@ -4,20 +4,25 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-int _putchar(char c);
-int _strlen(char *s);
-void _puts(char *str);
+/**
+ * specicif_fto - the funtion is struct of the typedef struct
+ * @fto: the identificator to format
+ * @p: called ot the funtion punters
+ */
 
-typedef struct {
-    char *fto;
-    void (*p)(va_list);
+typedef struct
+{
+	char *fto;
+	void (*p)(va_list);
 } specif_fto;
 
 int _printf(const char *format, ...);
-
 int _print_char(va_list args);
 int _print_int(va_list args);
 int _print_rev(va_list args);
 int _print_string(va_list args);
+int _putchar(char c);
+int _strlen(char *s);
+void _puts(char *str);
 
 #endif
