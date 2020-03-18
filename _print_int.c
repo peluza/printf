b4071a0cca144dba9b883960ffc3a7d7f380f76a
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-03-18 17:40:37 +0000

    fixing_file

--- _print_int.c
+++ _print_int.c
@@ -6,30 +6,30 @@
  */
 int _print_int(va_list args)
 {
-    int i = 0;
-    int mod = 1;
-    int digit;
-    unsigned int dig;
+	int i = 0;
+	int mod = 1;
+	int digit;
+	unsigned int dig;
 
-    digit = va_arg(args, int);
-    if (digit < 0)
-    {
-        _putchar('-');
-        dig = digit * -1;
-        i++;
-    }
-    else
-        dig = digit;
-    while (dig / mod > 9)
-    {
-        mod = mod * 10;
-    }
-    while (mod > 0)
-    {
-        _putchar(dig / mod + '0');
-        dig = dig % mod;
-        mod = mod / 10;
-        i++;
-    }
-    return (i);
+	digit = va_arg(args, int);
+	if (digit < 0)
+	{
+		_putchar('-');
+		dig = digit * -1;
+		i++;
+	}
+	else
+		dig = digit;
+	while (dig / mod > 9)
+	{
+		mod = mod * 10;
+	}
+	while (mod > 0)
+	{
+		_putchar(dig / mod + '0');
+		dig = dig % mod;
+		mod = mod / 10;
+		i++;
+	}
+	return (i);
 }
