b4cad2f0b12bd8c61aa9e5e7cc2b498a6812545f
peluza <edisonisaza@gmail.com>
2020-03-18 11:28:31 -0500

    print_a_string

---
+++ _puts.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *_puts - function prints characters and string
+ *@str: address at memory
+ */
+void _puts(char *str)
+{
+    int x;
+    for (x = 0; str[x] != '\0'; x++)
+    {
+        _putchar(str[x]);
+    }
+}
