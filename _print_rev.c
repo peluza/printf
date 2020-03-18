76bd178bb513eb4783aba4e21504052a3afa5fd5
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-03-18 16:34:53 +0000

    adding_file

---
+++ _print_rev.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _print_rev - print the text rev
+ *
+ * @args: caracter
+ *Return: number character
+ */
+int _print_rev(va_list args)
+{
+	char a;
+	char *s;
+	int x, c;
+
+	s = va_arg(args, char *);
+	a = s[0];
+	for (x = 0; a != '\0'; x++)
+	{
+		a = s[x];
+		c = x;
+	}
+	c = c - 1;
+	while (c >= 0)
+	{
+		_putchar(s[c]);
+		c--;
+	}
+	return (_strlen(s[c]));
+}
