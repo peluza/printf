2b84f7e004e70112ac464d0c2f37ecb49ee9eee1
peluza <edisonisaza@gmail.com>
2020-03-18 12:21:04 -0500

    print_a_string

--- _puts.c
+++ _puts.c
@@ -5,9 +5,10 @@
  */
 void _puts(char *str)
 {
-    int x;
-    for (x = 0; str[x] != '\0'; x++)
-    {
-        _putchar(str[x]);
-    }
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		_putchar(str[x]);
+	}
 }
